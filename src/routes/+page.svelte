d6374c11c084f9e00f7e451f3f744f6a2afde188
Zakrok09 <eccyboo@protonmail.com>
2023-04-17 20:18:01 +0200

    chore: setup tailwindcss and file structure

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+
+import ResultPanel from "../lib/components/ResultPanel.svelte";
+</script>
+
+<main>
+    <div class="left">
+        <ResultPanel />
+    </div>
+    <div class="right">
+
+    </div>
+</main>
