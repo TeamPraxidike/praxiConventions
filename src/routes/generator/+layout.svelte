e8ccc66d7f7051d198bf01a894697887056c1e69
Zakrok09 <eccyboo@protonmail.com>
2023-04-20 01:43:33 +0200

    feat: introduce a header

--- src/routes/generator/+layout.svelte
+++ src/routes/generator/+layout.svelte
@@ -1,3 +1,4 @@
+<!-- TODO Add common meta entries -->
 <script lang="ts">
 
 </script>
--- src/routes/generator/conventions/+page.svelte
+++ src/routes/generator/conventions/+page.svelte
@@ -3,7 +3,10 @@
     import ConventionsPicker from "$lib/components/editors/ConventionsPicker.svelte";
 </script>
 
-<div class="flex flex-col-reverse xl:flex-row gap-10 lg:gap-0 p-10">
+<h1 class="text-center pt-16 pb-5">CONVENTIONS.md Generator</h1>
+<p class="text-center"> Developed by <a class="unstyled text-primary-400 underline" href="https://github.com/Zakrok09">ecstasy</a> - member of <a class="unstyled text-primary-400 underline" href="https://github.com/TeamPraxidike">Praxidike</a></p>
+<p class="text-center">Credit: <a class="unstyled text-primary-400 underline" href="https://github.com/paulmis">paulmis</a></p>
+<div class="flex flex-col-reverse pt-14 xl:flex-row gap-10 lg:gap-0 p-10">
     <div class="left w-full xl:w-6/12">
         <ResultPanel />
     </div>
