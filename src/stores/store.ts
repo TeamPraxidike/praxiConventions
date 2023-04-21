b1700cc1effccaa88064d6d476ad69ae0d4cdc0e
Zakrok09 <eccyboo@protonmail.com>
2023-04-21 13:05:28 +0200

    refactor: clean up unused conventions class

--- src/stores/store.ts
+++ src/stores/store.ts
@@ -1,8 +1,6 @@
 import {writable} from "svelte/store";
 import {ConventionsPayload} from "../typescript/classes/ConventionsPayload";
-import {Convention} from "../typescript/classes/Convention";
 import {ContributionPayload} from "../typescript/classes/ContributionPayload";
 
 export const conventionsPayload = writable(new ConventionsPayload());
 export const contributionsPayload = writable(new ContributionPayload());
-export const convention = writable(new Convention(0,0,0,false));
