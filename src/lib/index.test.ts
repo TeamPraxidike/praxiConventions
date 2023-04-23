06917db919725d819f33027f87224817a06ab475
Zakrok09 <eccyboo@protonmail.com>
2023-04-24 00:10:38 +0200

    test: remove previous tests

--- src/lib/index.test.ts
+++ src/lib/index.test.ts
@@ -13,7 +13,3 @@ describe('Test Card.svelte', async () => {
         expect(screen.getByText('Inspiration desc')).toBeInTheDocument();
     })
 });
-
-describe('Test Conventions editor', async () => {
-
-})
